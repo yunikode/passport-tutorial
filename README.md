609256165a5a89bfe54b5e173e9152bc8d8d0624
zee-german <franz.lutzenberger@gmail.com>
2016-05-02 14:06:13 +0200

    made server https-only

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # passport-tutorial
+
+To run the server you need to create the cert/key pair called `my.crt` and `my.key`
+
+## For a simple self-signed key
+```openssl req -x509 -nodes -days 365 -newkey rsa:1024 -out my.crt -keyout my.key```
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var fs = require('fs')
+var https = require('https')
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
@@ -5,8 +8,14 @@ var expressSession = require('express-session')
 
 var passport = require('passport')
 var passportLocal = require('passport-local')
+var passportHttp = require('passport-http')
 
 var app = express()
+
+var server = https.createServer({
+  cert: fs.readFileSync(__dirname + '/my.crt'),
+  key: fs.readFileSync(__dirname + '/my.key')
+}, app)
 
 app.set('view engine', 'ejs')
 
@@ -22,13 +31,18 @@ app.use(expressSession({
 app.use(passport.initialize())
 app.use(passport.session())
 
-passport.use(new passportLocal.Strategy(function(username, password, done) {
- if ( username === password) {
-   done(null, { id: username, name: username })
- } else {
-   done(null, null)
- }
-}))
+passport.use(new passportLocal.Strategy(verifyCredentials))
+
+passport.use(new passportHttp.BasicStrategy(verifyCredentials))
+
+function verifyCredentials(username, password, done) {
+  // Pretend this is using a real database
+  if ( username === password) {
+    done(null, { id: username, name: username })
+  } else {
+    done(null, null)
+  }
+}
 
 passport.serializeUser(function(user, done) {
   done(null, user.id)
@@ -39,6 +53,14 @@ passport.deserializeUser(function(id, done) {
   done(null, { id: id, name: id })
 })
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next()
+  } else {
+    res.send(403)
+  }
+}
+
 app.get('/', function(req, res) {
   res.render('index', {
     isAuthenticated: req.isAuthenticated(),
@@ -46,7 +68,7 @@ app.get('/', function(req, res) {
   })
 })
 
-app.get('/login', function(req,res) {
+app.get('/login', function(req, res) {
   res.render('login')
 })
 
@@ -54,13 +76,23 @@ app.post('/login', passport.authenticate('local'), function(req, res) {
   res.redirect('/')
 })
 
-app.get('/logout', function(req,res) {
+app.get('/logout', function(req, res) {
   req.logout()
   res.redirect('/')
 })
 
+app.use('/api', passport.authenticate('basic', { session: false }))
+
+app.get('/api/data', ensureAuthenticated, function(req, res) {
+  res.json([
+    { value: 'foo' },
+    { value: 'bar' },
+    { value: 'baz' }
+  ])
+})
+
 var port = process.env.PORT || 1337
 
-app.listen(port, function() {
+server.listen(port, function() {
   console.log('http://127.0.0.1:' + port + '/')
 })
