0006b1276572b12819c91b1e16634dacfee43a58
zee-german <franz.lutzenberger@gmail.com>
2016-05-02 09:34:44 +0200

    installed required middleware

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var expressSession = require('express-session')
+
+var passport = require('passport')
+var passportLocal = require('passport-local')
 
 var app = express()
 
 app.set('view engine', 'ejs')
+
+<!-- MUST BE BEFORE PASSPORT -->
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(cookieParser())
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET || 'moo',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', function(req, res) {
   res.render('index', {
